409581402580b8eed61c9aa023fc73d922597dad
csimo005 <csimo005@ucr.edu>
2014-11-21 17:13:27 -0800

    added optional argument to specify number of files to transfer at a time.

--- transfer2.sh
+++ transfer2.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-SOURCE=$1
-DESTINATION=$2
+re='^[0-9]+$'
+if ! [[ $1 =~ $re ]] ; then
+   SOURCE=$1
+   DESTINATION=$2
+   else
+   NUM=$1
+   SOURCE=$2
+   DESTINATION=$3
+fi
 
 #Note: IP and login name must be changed for whatever it ends up being used for
 #Finds all the differences between the two directories and stores in array
@@ -10,11 +17,24 @@ myArr=($(diff one.txt two.txt | awk '{if($1~"<") print $2}'))
 
 #creates dir for the files to be cp into
 ssh cody@192.168.1.167 "mkdir $SOURCE/transfer"
-for i in "${myArr[@]}"
-do
-   #copies all files into transfer recursively
-   ssh cody@192.168.1.167 "cd $SOURCE && cp $i ./transfer && echo $i"
-done
+if ! [[ $1 =~ $re ]] ; then
+      for i in "${myArr[@]}"
+      do
+         #copies all files into transfer
+         ssh cody@192.168.1.167 "cd $SOURCE && cp $i ./transfer && echo $i"
+      done
+   else
+      i="0"
+      while [ $i -lt $NUM ]
+      do
+      if [ ${myArr[$i]} ]
+      then
+         #copies NUM amount of files into transfer
+         ssh cody@192.168.1.167 "cd $SOURCE && cp ${myArr[$i]} ./transfer && echo ${myArr[$i]}"
+      fi
+      i=$[$i+1]
+      done
+fi
 
 #tar and transfer directory
 ssh cody@192.168.1.167 "cd $SOURCE && tar -zcvf transfer.tar.gz transfer"
@@ -22,11 +42,24 @@ ssh cody@192.168.1.167 "scp $SOURCE/transfer.tar.gz cody@192.168.1.32:$DESTINATI
 
 #untar directory and moves it into destination dir
 tar -xf $DESTINATION/transfer.tar.gz
+mv transfer $DESTINATION
 rm $DESTINATION/transfer.tar.gz
-for i in "${myArr[@]}"
-do
-   mv $DESTINATION/transfer/$i $DESTINATION
-done
+if ! [[ $1 =~ $re ]] ; then
+      for i in "${myArr[@]}"
+      do
+         mv $DESTINATION/transfer/$i $DESTINATION
+      done
+   else
+      i="0"
+      while [ $i -lt $NUM ]
+      do
+         if [ ${myArr[$i]} ]
+         then
+            mv $DESTINATION/transfer/${myArr[$i]} $DESTINATION
+         fi
+         i=$[$i+1]
+      done
+fi
 
 #clean up
 ssh cody@192.168.1.167 "cd $SOURCE; rm transfer.tar.gz; rm -r transfer"
