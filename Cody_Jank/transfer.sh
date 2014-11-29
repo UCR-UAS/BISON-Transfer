b268f34e80f4f9832f2eed6c621858a6f41722b4
Brandon Lu <blu006@ucr.edu>
2014-11-28 17:00:58 -0800

    Removing cody jank for jank prealpha release

--- Cody_Jank/transfer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-SOURCE=$1
-DESTINATION=$2
-ssh cody@192.168.1.40 "tar -zcvf transfer.tar.gz /$SOURCE"
-ssh cody@192.168.1.40 "scp /$SOURCE/transfer.tar.gz cody@192.168.1.32:/$DESTINATION"
