fef9f812c17a27180d9c0180c4e3a50633189e88
Brandon Lu <brandon@mysteryBox>
2015-06-03 02:29:51 -0700

    oops.  address of c is passed instead.

--- src/BISON-Transferd.cpp
+++ src/BISON-Transferd.cpp
@@ -236,7 +236,7 @@ void handle_connection()
 		printf("Spawned child exiting.\n");
 		shutdown(cfd, SHUT_WR);
 		std::cout << "Connection shutdown." << std::endl;
-		while(read(cfd, c, 1) == 0);
+		while(read(cfd, &c, 1) == 0);
 		std::cout << "Connection reading..." << std::endl;
 		close(cfd);
 		std::cout << "Connection closed." << std::endl;
